35fa5602784c4ab06d4ce7e7ac4f04a44cc4aeb2
Clara Hughes <chughes@cssienroll.com>
2017-02-17 16:50:56 -0500

    first page is done - ST

--- app/assets/script/App.js
+++ app/assets/script/App.js
@@ -2,13 +2,20 @@ var $ = require('jquery');
 
 var Person = require('./modules/Person');
 
-alert("ABC 321--- 1, 2 3!");
+class Adult extends Person {
+    payTaxes(){
+        colsole.log(this.name + "now owes $0 in taxes.");
+      
+    }
+}
+
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
 var jane = new Person("Jane Smith", "reddish");
 jane.greet();
+jane.payTaxes();
 
 
-$("h1").remove();
+
